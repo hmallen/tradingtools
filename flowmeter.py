62aef43a236c901e66f693c0b5fefe2706d886c6
hmallen <allenhm@gmail.com>
2018-07-19 19:41:11 -0500

    Small changes.

--- flowmeter.py
+++ flowmeter.py
@@ -1,11 +1,22 @@
+import configparser
 import logging
 import os
 import sys
+
+from binance.client import Client as BinanceClient
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+config_path = 'config/config.ini'
+
 
 if __name__ == '__main__':
-    pass
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    binance_api = config['binance']['api']
+    binance_secret = config['binance']['secret']
+
+    binance_client = BinanceClient(binance_api, binance_secret)
